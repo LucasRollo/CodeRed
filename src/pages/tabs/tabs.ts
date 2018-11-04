e400405cdfe585ecd0b10929895ddc7d28f7a0be
rubenuribe-2 <uriben2000@gmail.com>
2018-11-03 20:03:02 -0500

    Changed Names

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { ProfilePage } from '../Profile/Profile';
+import { FriendsPage } from '../Friends/Friends';
+import { OpenPage } from '../Open/Open';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = OpenPage;
+  tab2Root = ProfilePage;
+  tab3Root = FriendsPage;
 
   constructor() {
 
